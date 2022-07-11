f5b3b3e6e57c329b80d54c834df93938637d117f
udovenko-ms <udovenko-ms@localhost.localdomain>
2022-07-11 20:11:11 +0500

    fix branch

--- src/top.v
+++ src/top.v
@@ -1,19 +1,15 @@
 module top(
 
 input clk,
-input bit0, bit1, bit2, bit3,
-input summ4, vixit5, delen6, umnog7,
-input k_1, k_2
 
-output anodes_1, anodes_2, anodes_3, anodes_4,
-output seg_1, seg_2, seg_3, seg_4, seg_5, seg_6, seg_7
+output [3:0]anodes,
+output [6:0]segments
 );
 
-bit bit(.bit0(bit0), .bit1(bit1), .bit2(bit2), .bit3(bit3), .k_1(k_1), .k_2(k_2), .reg_1(reg_1), .reg_2(reg_2));
+reg [7:0] d = 225;
 
-Arif Arif(.summ4(summ4), .vixit5(vixit5), .delen6(delen6), .umnog7(sw_7), .first(reg_1), .second(reg_2), .final(final));
+clk_div clk_div(.clk(clk), .clk2(clk2));
 
-Ind Ind(.clk(clk), .anodes_1(anodes_1), .anodes_2(anodes_2), .anodes_3(anodes_3), .anodes_4(anodes_4), .seg_1(seg_1), .seg_2(seg_2), 
-.seg_3(seg_3), .seg_4(seg_4), .seg_5(seg_5), .seg_6(seg_6), .seg_7(seg_7));
+segment segment(.clk(clk2), .data(d), .anodes(anodes), .segments(segments));
 
 endmodule
